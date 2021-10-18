b8be2a9ac0baa68dd824daf0f5cf98032df8267e
xchiron <miked.huang@gmail.com>
2021-10-18 00:12:13 -0500

    remove onCreateNode consoleLogs

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,6 @@ const path = require("path")
 
 exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions
-  console.log(node.internal.type + " " + node.relativePath) //JSON.stringify(node))
   if (node.internal.type === "MarkdownRemark") {
     const slug = path.basename(node.fileAbsolutePath, ".md")
     console.log(slug)
